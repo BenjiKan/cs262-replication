cbbaaccfc0253ae4bb7125d5853836939fe3306f
mosesmayer <mosesmayer@gmail.com>
2023-02-22 15:52:36 -0500

    cleanup

--- client.py
+++ client.py
@@ -360,37 +360,6 @@ def Main():
 		pass
 	s.close()
 
-def bankMain():
-	# host and port defined in constants
-
-	s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-
-	# connect to server on local computer
-	s.connect((HOST, PORT))
-
-	# message you send to server
-	#message = "V for vendetta"
-	while True:
-		# message sent to server
-		# message received from server
-		# ask the client whether he wants to continue
-		ans = input('\nEnter your request:')
-		if ans == '':
-			ans2 = input('\nDo you want to continue(y/n) :')
-			if ans2 =='y':
-				continue
-			else:
-				break
-		else:
-			s.send(ans.encode('ascii'))
-			data = s.recv(1024)
-			# print the received message
-			# here it would be a reverse of sent message
-			print('Received from the server :',str(data.decode('ascii')))
-			continue
-	# close the connection
-	s.close()
-
 if __name__ == '__main__':
 	Main()
 
